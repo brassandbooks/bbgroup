9a6571bb12547db374d54d0e62c1a2df99155d16
Whyte Peter <peteremmanuelwhyte@gmail.com>
2020-09-10 15:23:01 +0100

    almost done

--- js/main.js
+++ js/main.js
@@ -1,31 +1,16 @@
 $(document).ready(function(){
 
- const cardFront = document.querySelectorAll('.front')
- const cardBack = document.querySelectorAll('.back')
- 
-function toggleCard(c, s){
-    if(c.className.includes('activeCard')){
-        c.classList.remove('activeCard')
-        s.classList.add('activeCard')
-      }
-     else {
-        c.classList.add('activeCard')
-        s.classList.remove('activeCard')
-     }
-}
- cardFront.forEach( card => {
-     card.addEventListener('mouseenter', () => {
-         id = card.id
-         cardBack.forEach( seen => {
-             if(seen.id === id){
-                toggleCard(card, seen)
-               seen.addEventListener('mouseleave', ()=> {
-                toggleCard(card, seen)
-               })
-             }
-         })
-     })
- })
+    let links = document.querySelectorAll('.nav-item')
 
- 
+
+    links.forEach(link => {
+        link.addEventListener('click', e => {        
+            links.forEach(l => {
+                if(l.className.includes('active')){
+                    l.classList.remove('active')
+                }
+                e.target.parentNode.classList.add('active')
+            });
+        })
+    })
 })
